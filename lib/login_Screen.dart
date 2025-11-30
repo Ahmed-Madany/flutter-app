22b965cb82319f08658ed3db1eb6d75fc6cac671
Ahmed-Madany <madanyo85@gmail.com>
2025-11-30 23:41:33 +0200

    assets

--- lib/login_Screen.dart
+++ lib/login_Screen.dart
@@ -1,4 +1,3 @@
-
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
@@ -21,18 +20,43 @@ class _SignInScreenState extends State<SignInScreen> {
         title: Text("Login screen"),
       ),
       body: Column(
-        children: [Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("Login screen"),
-        ),
-        Padding(padding: const EdgeInsets.all(8.0),child:CustomTextField(lableText: "Email"),),
-        CustomTextField(lableText: "Password"),ElevatedButton(onPressed: (){
-       Navigator.of(context).push(
-    MaterialPageRoute<void>(
-      builder: (context) => const HomeScreen(),
-    ),
-  );
-        }, child: Text("Login"))],
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Image.asset("assets/icon1.jpg"),
+                ),
+
+                SizedBox(
+                  height: 150,
+                  width: 150,
+                  child: Image.network(
+                    "https://i.pinimg.com/736x/01/78/af/0178af13e6fe792ecd15d2a27583033e.jpg",
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: CustomTextField(lableText: "Email"),
+          ),
+          CustomTextField(lableText: "Password"),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute<void>(
+                  builder: (context) => const HomeScreen(),
+                ),
+              );
+            },
+            child: Text("Login"),
+          ),
+        ],
       ),
     );
   }
